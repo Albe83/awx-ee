a328334a4fc8946d70396175aca4feedf442fdf6
Alberto Clemente <alberto.clemente@sisal.it>
2022-11-11 23:32:00 +0100

    build script

--- build.sh
+++ build.sh
@@ -1,137 +1,62 @@
 #!/bin/bash
-#------------------------------------------------------------------------------------
-# What: A quick-start BASH template script
-# Author: Adam Hayward <adam at happy dot cat>
-# License: http://svn.happy.cat/public/LICENSE.txt
-#
-# Features:
-#   log "A log message"
-#   debug "A debug message only displayed if '-v' option passed"
-#   error "A fatal error message and exit with code:" 2
-#   usage - print a usage message
-#   get_options - parse command-line options, defaults below
-#
-# Standard command-line options:
-#   -v verbose
-#   -h help message
-#   -V version number
-# Long-format options:
-#   --version
-#   --help  
-#
-#------------------------------------------------------------------------------------
-#
-# example-script.sh - Short description
-# 
-# author:       Author of script
-# contact:	    Email
-# since:        Date
-# 
-#------------------------------------------------------------------------------------
+export GIT_REPO_AWX_EE="https://github.com/Albe83/awx-ee.git"
 
-#------------------------------------------------------------------------------------
-# SCRIPT CONFIGURATION
-#------------------------------------------------------------------------------------
-
-SCRIPT_NAME=`basename $0`
-
-# If debug information should be shown
-VERBOSE=
-
-VERSION=0.1
-
-# Add your own global variables here
-export GIT_REPO_AWX_EE=""
-
-export DOCKER_HUB_USERNAME=""
-export DOCKER_HUB_PASSWORD=""
+export DOCKER_HUB_USERNAME="albe83"
+export DOCKER_HUB_PASSWORD="xxxx"
 
 export TOWER_HOST="http://127.0.0.1:30080"
 export TOWER_USERNAME="admin"
-export TOWER_PASSWORD=""
+export TOWER_PASSWORD="zzz"
 
 export EE_IMAGE_NAME="custom-awx-ee"
 export EE_IMAGE_VERSION_MAJOR="0"
 export EE_IMAGE_VERSION_MINOR="0"
-export EE_IMAGE_VERSION_PATCH="0"
+export EE_IMAGE_VERSION_PATCH="1"
 
-#------------------------------------------------------------------------------------
-# UTILITY FUNCTIONS
-#------------------------------------------------------------------------------------
+###################
+git clone "$GIT_REPO_AWX_EE"
+cd awx-ee
 
-# print a log a message
-log ()
-{
-    echo "[${SCRIPT_NAME}]: $1" > /dev/stderr
-}
+###################
+export EE_IMAGE_NAME_MAJOR="$EE_IMAGE_NAME:v$EE_IMAGE_VERSION_MAJOR"
+export EE_IMAGE_NAME_MINOR="$EE_IMAGE_NAME_MAJOR.$EE_IMAGE_VERSION_MINOR"
+export EE_IMAGE_NAME_PATCH="$EE_IMAGE_NAME_MINOR.$EE_IMAGE_VERSION_PATCH"
 
-# print a debug message - only outputs is VERBOSE flag is set
-debug()
-{
-    [ "$VERBOSE" ] && echo "[${SCRIPT_NAME}]: $1" > /dev/stderr
-}
+export EE_IMAGE_FULLNAME="$DOCKER_HUB_USERNAME/$EE_IMAGE_NAME"
+export EE_IMAGE_FULLNAME_MAJOR="$DOCKER_HUB_USERNAME/$EE_IMAGE_NAME_MAJOR"
+export EE_IMAGE_FULLNAME_MINOR="$DOCKER_HUB_USERNAME/$EE_IMAGE_NAME_MINOR"
+export EE_IMAGE_FULLNAME_PATCH="$DOCKER_HUB_USERNAME/$EE_IMAGE_NAME_PATCH"
 
-# print an error message and exit()
-error()
-{
-    echo "[${SCRIPT_NAME}] ERROR: $1" > /dev/stderr
-    [ $# -gt 1 ] && exit $2
-    exit 1
-}
+ansible-builder build \
+  --container-runtime docker \
+  --prune-images \
+  --tag "$EE_IMAGE_FULLNAME_MAJOR" \
+  --tag "$EE_IMAGE_FULLNAME_MINOR" \
+  --tag "$EE_IMAGE_FULLNAME_PATCH"
 
-# Define your own script functions here
+###################
+docker login --username="$DOCKER_HUB_USERNAME" --password="$DOCKER_HUB_PASSWORD"
+docker push --all-tags "$EE_IMAGE_FULLNAME"
+docker logout
 
-# Print a usage message
-usage()
-{
-cat << USAGE
-usage: $0 [-v] [-h] -a op1 -b op2
+###################
+cd ..
+docker rmi "$EE_IMAGE_FULLNAME_MAJOR" "$EE_IMAGE_FULLNAME_MINOR" "$EE_IMAGE_FULLNAME_PATCH"
+rm -fR awx-ee
 
-Short description
+###################
+export AWX_LOGIN_TOKEN_DESC="AWX CLI - $(date "+%Y%m%d%H%M%S%N")"
+$(awx login -f human --description "$AWX_LOGIN_TOKEN_DESC")
 
-REQUIRED OPTIONS:
-    -a op1      Option 1
-    -b op2      Option 2
-OTHER OPTIONS:
-    -v         Show debuging messages
-    -h         Show this help message
-    -V         Show version
-USAGE
-}
+export AWX_EE_NAME="${EE_IMAGE_NAME^^}"
+export AWX_EE_NAME_MAJOR="$AWX_EE_NAME (v$EE_IMAGE_VERSION_MAJOR)"
+export AWX_EE_NAME_MINOR="$AWX_EE_NAME (v$EE_IMAGE_VERSION_MAJOR.$EE_IMAGE_VERSION_MINOR)"
+export AWX_EE_NAME_PATCH="$AWX_EE_NAME (v$EE_IMAGE_VERSION_MAJOR.$EE_IMAGE_VERSION_MINOR.$EE_IMAGE_VERSION_PATCH)"
 
-# Get the script options
-get_options()
-{
-    while getopts "a:b:hvV-:" OPTION
-    do
-        if [ $OPTION == "-" ]; then
-            OPTION=$OPTARG
-        fi
-        case $OPTION in
-            'git-repo')  GIT_REPO_AWX_EE=${OPTARG};;
-            'docker-hub-user')  DOCKER_HUB_USERNAME=${OPTARG};;
-            'docker-hub-password')  DOCKER_HUB_PASSWORD=${OPTARG};;
-            'awx-host')  TOWER_HOST=${OPTARG};;
-            'awx-user')  TOWER_USERNAME=${OPTARG};;
-            'awx-password')  TOWER_PASSWORD=${OPTARG};;
-            'ee-image-name')  EE_IMAGE_NAME=${OPTARG};;
-            'ee-version-major')  EE_IMAGE_VERSION_MAJOR=${OPTARG};;
-            'ee-version-minor')  EE_IMAGE_VERSION_MINOR=${OPTARG};;
-            'ee-version-patch')  EE_IMAGE_VERSION_PATCH=${OPTARG};;
-            h)  usage && exit 0;;
-       'help')  usage && exit 0;;
-            V)  echo $VERSION && exit 0;;
-    'version')  echo $VERSION && exit 0;;
-            v)  VERBOSE=1;;
-            \?) echo "Invalid option" && usage && exit 1;;
-        esac
-    done
-}
+awx execution_environments delete "$AWX_EE_NAME_MAJOR"
+awx execution_environments delete "$AWX_EE_NAME_MINOR"
+awx execution_environments delete "$AWX_EE_NAME_PATCH"
 
-main()
-{
-    get_options "$@"
-    # Put the rest of your main script here
-}
-
-main "$@"
+awx execution_environments create --name "$AWX_EE_NAME_MAJOR" --image "$EE_IMAGE_FULLNAME_MAJOR" --pull "always"
+awx execution_environments create --name "$AWX_EE_NAME_MINOR" --image "$EE_IMAGE_FULLNAME_MINOR" --pull "always"
+awx execution_environments create --name "$AWX_EE_NAME_PATCH" --image "$EE_IMAGE_FULLNAME_PATCH" --pull "always"
